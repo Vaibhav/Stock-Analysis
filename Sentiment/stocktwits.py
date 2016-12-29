711a82a3063ed2ce29dde39f457c73f9f84d64c0
Vaibhav <ishuvaibhav@gmail.com>
2016-12-29 00:38:33 -0400

    Added proper comments and tested code thoroughly

--- Sentiment/stocktwits.py
+++ Sentiment/stocktwits.py
@@ -4,7 +4,15 @@ import json
 import datetime
 
 
-def get_tweets(ticker):
+# write to JSON file and properly formats the file
+def write_to_file(nameOfFile, data):
+    with open(nameOfFile, 'w+') as f:
+        print "Dumping JSON to", nameOfFile
+        json.dump(data, f, sort_keys=True, indent=4, separators=(',', ':'))
+
+
+# get the data using StockTwits API
+def get_twits(ticker):
     url = "https://api.stocktwits.com/api/2/streams/symbol/{0}.json".format(ticker)
     connection = urllib2.urlopen(url)
     data = connection.read()
@@ -12,12 +20,14 @@ def get_tweets(ticker):
     return json.loads(data)
 
 
-def get_tweets_list(tickers):
+# loops through to get data for each ticker in the tickers list
+def get_twits_list(tickers):
     ret = {}
     for ticker in tickers:
         print "Getting data for", ticker
+        # error handling
         try:
-            data = get_tweets(ticker)
+            data = get_twits(ticker)
             symbol = data['symbol']['symbol']
             msgs = data['messages']
             ret.update({symbol: msgs})
@@ -27,10 +37,24 @@ def get_tweets_list(tickers):
     return ret
 
 
+def read_tickers():
+    print "Reading tickers from \"tickers.txt\":"
+    f = open("tickers.txt", 'r')
+    names = []
+    # read tickers from tickers.txt
+    for line in f:
+        line = line.strip('\n')
+        line = line.strip('\t')
+        names.append(line);
+    print names
+    return names
+
+
 def remove_old(original, age_limit=30):
     print "Removing tweets that are more than", age_limit, "days old"
     threshold = datetime.datetime.now() - datetime.timedelta(age_limit)
     result = {}
+    # checks if data is more than age_limit and removes if so
     for ticker in original.keys():
         result[ticker] = []
         for msg in original[ticker]:
@@ -39,39 +63,28 @@ def remove_old(original, age_limit=30):
                 result[ticker].append(msg)
     return result
 
-
-def write_to_file(filename, data):
-    with open(filename, 'w+') as f:
-        print "Dumping JSON to", filename
-        json.dump(data, f, sort_keys=True, indent=4, separators=(',', ':'))
-
-
-def read_tickers():
-    print "Reading tickers from \"tickers.txt\":"
-    f = open("tickers.txt", 'r')
-    names = []
-    for line in f:
-        line = line.strip('\n')
-        line = line.strip('\t')
-        names.append(line);
-    print names
-    return names
-
-FILENAME = "stocktwits.json"  # change as necessary
+# default file name
+FILENAME = "stocktwits.json"
 
 if __name__ == "__main__":
 
+    # Optional file output
     print "Do you want to specify name of file?"
     x = raw_input()
 
+    # Execute this code if option is taken
     if x.startswith("y") or x.startswith("Y"):
         print "What is file name?"
         filename = raw_input()
         if not (filename.endswith(".json")):
-            filename = filename + ".json";
-        FILENAME = filename;
+            filename = filename + ".json"
+        FILENAME = filename
 
-    codes = read_tickers();
-    twitdata = get_tweets_list(codes);
-    twitdata = remove_old(twitdata);
-    write_to_file(FILENAME, twitdata);
+    # get list of ticker codes
+    codes = read_tickers()
+    # for each ticker code get the data
+    twitdata = get_twits_list(codes)
+    # remove the data if older than X days, useless
+    twitdata = remove_old(twitdata, 21)
+    # write to json file
+    write_to_file(FILENAME, twitdata)
