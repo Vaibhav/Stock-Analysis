f0e00720cd03edb5c511024290d08d5d01734b26
Vaibhav <ishuvaibhav@gmail.com>
2017-12-25 19:02:56 -0400

    working on trendline generator

--- Patterns/TrendLine.py
+++ Patterns/TrendLine.py
@@ -5,8 +5,22 @@ import pandas.io.data as pd
 def trendGen(xDat, window=1.0/3.0, charts=True): 
     
     x = np.array(xDat)
-
-    window = window * len(x)
+    xLen = len(x)
+    window = window * xLen
     window = int(window)
 
+    # find index of min and max
+    absMax = np.where(x == max(x)[0][0]) 
+    absMin = np.where(x == min(x)[0][0])
+
+    if absMax + window > xLen:
+        xmax = max(x[0:(absMax - window)])
+    else:
+        xmax = max(x[(absMax + window)])
+        
     
+    if absMin - window < 0:
+        xmin = min(x[(absMin + window)])
+    else:
+        xmin = min(x[0:(absMin - window)])
+
