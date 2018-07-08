4ae9d80d03fe1b071589cd9c25b9cb572db371a6
Jack Moody <jacklaytonmoody@gmail.com>
2018-07-08 17:50:41 -0400

    Updating to Python3 and Simplifying (#11)
    * Start updating to Python 3
    
    * Update to Python3 and simplify GET request with requests module
    
    * Remove manual testing code
    
    * Update to Python3 with requests module for GET request
    
    * Simplify requests
    
    * Update to Python3 and start PEP8 compliance
    
    * Reformat as tuples and update to Python3
    
    * Update to Python3

--- Regression/Linear Regression.py
+++ Regression/Linear Regression.py
@@ -1,37 +1,38 @@
-# Linear Regression for Stock based on Date 
+# Linear Regression for Stock based on Date
 # Currently only works for month
 # http://scikit-learn.org/stable/auto_examples/linear_model/plot_ols.html#example-linear-model-plot-ols-py
 
 import csv
-import numpy as np 
+import numpy as np
 from sklearn import datasets, linear_model
 import matplotlib.pyplot as plt
 
 close_prices = []
-dates = [] 
+dates = []
 
-f = raw_input('Enter filename: \n')
+f = input('Enter filename: \n')
 
 if not(f.endswith('.csv')):
-	f = f + '.csv'
-	
+    f = f + '.csv'
+
 with open(f, 'r') as file:
-	the_data = csv.reader(file);
-	the_data.next()
-	
-	for row in the_data:
-		x = float(row[4])
-		close_prices.append(float("{0:.2f}".format(x)))
-		dates.append(int(row[0].split('/')[0]))
+    the_data = csv.reader(file)
+    the_data.next()
+
+    for row in the_data:
+        x = float(row[4])
+        close_prices.append(float("{0:.2f}".format(x)))
+        dates.append(int(row[0].split('/')[0]))
 
 
-#convert lists to numpy arrays
-length_of_dates = len(dates) + 1;
+# convert lists to numpy arrays
+length_of_dates = len(dates) + 1
 day = []
 day.extend(range(1, length_of_dates))
 
-rev = raw_input('Is the newest price first? (y/n)')
-if rev.startswith('y'): close_prices.reverse();
+rev = input('Is the newest price first? (y/n)')
+if rev.startswith('y'):
+    close_prices.reverse()
 
 prices_arr = np.reshape(close_prices, (len(close_prices), 1))
 days_arr = np.reshape(day, (len(day), 1))
@@ -39,7 +40,7 @@ days_arr = np.reshape(day, (len(day), 1))
 print close_prices
 print day
 
-#Creating lin reg object
+# Creating lin reg object
 regr = linear_model.LinearRegression()
 regr.fit(days_arr, prices_arr)
 
@@ -56,10 +57,11 @@ maxpr = max(close_prices)
 maxdt = max(day)
 mindt = min(day)
 
-#Draw black dots representing prices
-plt.figure(figsize=(15,15), dpi=240)
-plt.scatter(days_arr, prices_arr, color = 'black', label = 'Close Prices')
-plt.plot(days_arr, regr.predict(days_arr), color = 'red', linewidth = 4, label = 'Estimated Linear Function')
+# Draw black dots representing prices
+plt.figure(figsize=(15, 15), dpi=240)
+plt.scatter(days_arr, prices_arr, color='black', label='Close Prices')
+plt.plot(days_arr, regr.predict(days_arr), color='red',
+         linewidth=4, label='Estimated Linear Function')
 plt.xlabel('Day')
 plt.ylabel('Close Price')
 plt.title('Linear Regression')
@@ -71,5 +73,3 @@ plt.subplots_adjust(left=0.07)
 plt.subplots_adjust(right=0.96)
 plt.legend()
 plt.show()
-
-
