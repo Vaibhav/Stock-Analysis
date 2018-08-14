94da1f280eb8f7eec9e16530bda1935cdd83d154
Vaibhav <ishuvaibhav@gmail.com>
2018-08-14 18:01:48 -0300

    dos2unix

--- Patterns/TrendLine.py
+++ Patterns/TrendLine.py
@@ -5,23 +5,23 @@ from matplotlib.pyplot import plot, grid, show
 from pandas_datareader import data, wb
 import pandas as pd
 
-def trendGen(xDat, window=1.0/3.0, needPlot=True): 
-    
+def trendGen(xDat, window=1.0/3.0, needPlot=True):
+
     x = np.array(xDat)
     xLen = len(x)
     window = window * xLen
     window = int(window)
 
     # find index of min and max
-    absMax = np.where(x == max(x))[0][0] 
+    absMax = np.where(x == max(x))[0][0]
     absMin = np.where(x == min(x))[0][0]
 
     if absMax + window > xLen:
         xmax = max(x[0:(absMax - window)])
     else:
         xmax = max(x[(absMax + window):])
-        
-    
+
+
     if absMin - window < 0:
         xmin = min(x[(absMin + window):])
     else:
@@ -30,8 +30,8 @@ def trendGen(xDat, window=1.0/3.0, needPlot=True):
     xmax = np.where(x == xmax)[0][0]  # index of the 2nd max
     xmin = np.where(x == xmin)[0][0]  # index of the 2nd min
 
-    # Create the trend lines 
-    # rise over run 
+    # Create the trend lines
+    # rise over run
     slopeMax = (x[absMax] - x[xmax]) / (absMax - xmax)
     slopeMin = (x[absMin] - x[xmin]) / (absMin - xmin)
     amax     = x[absMax] - (slopeMax * absMax)
@@ -59,17 +59,17 @@ trendGen(dat, window = 1.0/3)
 
 
 def findTops(x, window=1.0/3, charts=True):
-    
+
     x = np.array(x)
     xLen = len(x)
 
     if window < 1:
         window = int(window * xLen)
-    
+
     sigs = np.zeros(xLen, dtype=float)
 
     i = window
-    
+
     while i != xLen:
         if x[i] > max(x[i-window:i]): sigs[i] = 1
         elif x[i] < min(x[i-window:i]): sigs[i] = -1
@@ -77,10 +77,10 @@ def findTops(x, window=1.0/3, charts=True):
 
     xmin = np.where(sigs == -1.0)[0]
     xmax = np.where(sigs == 1.0)[0]
-    
+
     ymin = x[xmin]
     ymax = x[xmax]
-    
+
     if charts is True:
         plot(x)
         plot(xmin, ymin, 'ro')
