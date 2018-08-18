90ec8b354821b5142131a828f21935e637045339
Vaibhav <ishuvaibhav@gmail.com>
2018-08-18 01:24:08 -0300

    Dataframe now being populated properly

--- Patterns/MACD.py
+++ Patterns/MACD.py
@@ -1,7 +1,7 @@
 '''
 Author: Vaibhav Khaitan
 Date: 8/17/2018
-Description: 
+Description:
 '''
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,5 +21,21 @@ df = get_historical_data(ticker, start=start, end=now, output_format='pandas')
 def macd(dat):
   dat['ma1'] = dat['close'].rolling(window=moving_avg1, min_periods=1).mean()
   dat['ma2'] = dat['close'].rolling(window=moving_avg2, min_periods=1).mean()
-    
+
   return dat
+
+def add_macd(df):
+
+  df = macd(df)
+  df['position'] = 0
+
+  df['position'][moving_avg1:] = np.where(df['ma1'][moving_avg1:] >= df['ma2'][moving_avg1:], 1, 0)
+
+  df['signals'] = df['position'].diff()
+  df['oscillator'] = df['ma1'] - df['ma2']
+
+  return df
+
+
+df = add_macd(df)
+print(df)
