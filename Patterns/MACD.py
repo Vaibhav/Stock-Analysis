36c038094982537dcff101b0a745d5f2c63c7618
Vaibhav <ishuvaibhav@gmail.com>
2018-08-22 17:48:03 -0300

    Need to add visualization

--- Patterns/MACD.py
+++ Patterns/MACD.py
@@ -1,14 +1,13 @@
 '''
 Author: Vaibhav Khaitan
 Date: 8/17/2018
-Description:
+Description: Creates a dataframe with moving averages and MACD oscillator
 '''
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from datetime import datetime, timedelta
-from pandas_datareader import data
-from iexfinance import Stock, get_historical_data, get_available_symbols
+from iexfinance import get_historical_data
 
 moving_avg1 = 10
 moving_avg2 = 20
@@ -20,8 +19,8 @@ df = get_historical_data(ticker, start=start, end=now, output_format='pandas')
 
 
 def macd(dat):
-  dat['ma1'] = dat['close'].rolling(window=moving_avg1, min_periods=1).mean()
-  dat['ma2'] = dat['close'].rolling(window=moving_avg2, min_periods=1).mean()
+  dat['10dma'] = dat['close'].rolling(window=moving_avg1, min_periods=1).mean()
+  dat['20dma'] = dat['close'].rolling(window=moving_avg2, min_periods=1).mean()
 
   return dat
 
@@ -31,13 +30,15 @@ def add_macd(df):
   df = macd(df)
   df['position'] = 0
 
-  df['position'][moving_avg1:] = np.where(df['ma1'][moving_avg1:] >= df['ma2'][moving_avg1:], 1, 0)
+  df['position'][moving_avg1:] = np.where(df['10dma'][moving_avg1:] >= df['20dma'][moving_avg1:], 1, 0)
 
   df['signals'] = df['position'].diff()
-  df['oscillator'] = df['ma1'] - df['ma2']
+  df['oscillator'] = df['10dma'] - df['20dma']
 
   return df
 
 
 df = add_macd(df)
-print(df)
+# print(df)
+print(df.loc[df['signals'] == 1])
+print(df.loc[df['signals'] == -1])
