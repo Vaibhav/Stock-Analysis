6b5e593879cd7759bb6b50fdd631ab97eab775bc
Vaibhav Khaitan <ishuvaibhav@gmail.com>
2020-10-11 22:00:20 -0300

    Rewrite svc.py (#15)

--- Prediction/svc.py
+++ Prediction/svc.py
@@ -1,36 +1,34 @@
 # Machine learning classification libraries
 from sklearn.svm import SVC
-from sklearn.metrics import scorer
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import scorer, accuracy_score
 
 # For data manipulation
 import pandas as pd
 import numpy as np
+from math import floor
 
 # To plot
 import matplotlib.pyplot as plt
 
 # To fetch data
 from pandas_datareader import data as pdr
-from iexfinance import get_historical_data
+import yfinance as yf
 from datetime import datetime
 
 # Fetch the data
-ticker = "AAPL"
-start = datetime(2015,1,1)
-now = datetime.now()
-
-df = get_historical_data(ticker, start=start, end=now, output_format='pandas')
+ticker = "MSFT"
+df = yf.Ticker(ticker).history(period="2y")
 df= df.dropna()
 
-y = np.where(df['close'].shift(-1) > df['close'],-1,1)
-df['Open-Close'] = df.open - df.close
-df['High-Low'] = df.high - df.low
+# Create features
+y = np.where(df['Close'].shift(-1) > df['Close'],1,-1)
+df['Open-Close'] = df.Open - df.Close
+df['High-Low'] = df.High - df.Low
 
-x=df[['Open-Close','High-Low']]
+x=df[['Open-Close','High-Low', 'High', 'Low', 'Volume']]
 
-split_percentage = 0.8
-split = int(split_percentage*len(df))
+split_percentage = 0.75
+split = int(floor(split_percentage*len(df)))
 
 # Train data set
 x_train = x[:split]
@@ -45,16 +43,22 @@ cls = SVC().fit(x_train, y_train)
 accuracy_train = accuracy_score(y_train, cls.predict(x_train))
 accuracy_test = accuracy_score(y_test, cls.predict(x_test))
 
-print('\nTrain Accuracy:{: .2f}%'.format(accuracy_train*100))
+print('Train Accuracy:{: .2f}%'.format(accuracy_train*100))
 print('Test Accuracy:{: .2f}%'.format(accuracy_test*100))
 
 df['Predicted_Signal'] = cls.predict(x)
 
-# Calculate log returns
-df['Return'] = np.log(df.close.shift(-1) / df.close)*100
-df['Strategy_Return'] = df.Return * df.Predicted_Signal
-df.Strategy_Return.iloc[split:].cumsum().plot(figsize=(10,5))
+# Calculate returns
+lag_price = df.Close.shift(1)
+
+df['Return'] = (df.Close - lag_price) / lag_price
+df['Strategy_Return'] = df.Return * df.Predicted_Signal.shift(1)
+
+# Plot results
+
+df.Strategy_Return.iloc[split:].cumsum().plot(figsize=(10,5))  # Strategy returns
+plt.plot(df.Return.iloc[split:].cumsum()) # buy and hold
 plt.ylabel("Strategy Returns (%)")
 plt.show()
 
-print(df)
+# print(df)
